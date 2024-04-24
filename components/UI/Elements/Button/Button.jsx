a0e15b82e379fef95f68cb9573faa92b3f7437bd
Cyril Gouv <hello@cyrilgouv.com>
2024-04-24 09:46:58 +0200

    added button component

--- components/UI/Elements/Button/Button.jsx
+++ components/UI/Elements/Button/Button.jsx
@@ -1,10 +1,41 @@
+import Link from "next/link"
+
 import styles from "./Button.module.scss"
 
 
 const Button = ({ text, path, external = false }) => {
-    return (
-        <div>Button</div>
-    )
+
+    if(!external) {
+        return (
+            <Link
+                href={ path }
+                className={ `${styles.button} text--regular` }
+            >
+                <span 
+                    data-name={ text }
+                    className={ styles.button__text } 
+                >
+                    { text }
+                </span>
+            </Link>
+        )
+    } else {
+        return (
+            <a
+                href={ path }
+                target="_blank"
+                rel="noopener noreferrer"
+                className={ `${styles.button} text--regular` }
+            >
+                <span 
+                    data-name="text"
+                    className={ styles.button__text } 
+                >
+                    { text }
+                </span>
+            </a>
+        )
+    }
 }
 
 export default Button
