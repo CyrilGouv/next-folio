f62d3732d75bfde49ed82ca36504609c7e2150d6
Cyril Gouv <hello@cyrilgouv.com>
2024-05-06 09:01:48 +0200

    added Legales Content component

--- components/Blocks/Legales/Legales.jsx
+++ components/Blocks/Legales/Legales.jsx
@@ -1,8 +1,9 @@
 import TextInfo from "@/components/UI/Elements/Text/TextInfo"
+import LegalesContent from "./LegalesContent"
 import { mentionsContent } from "@/data/content"
 
 import styles from "./Legales.module.scss"
-import LegalesContent from "./LegalesContent"
+import Line from "@/components/UI/Elements/Line/Line"
 
 
 const Legales = () => {
@@ -10,15 +11,23 @@ const Legales = () => {
         <section className={ styles.legales }>
             <div className="container">
 
+                <div className={ styles.legales__line }>
+                    <Line type="inner" />
+                </div>
+
                 { mentionsContent.map(({ id, title, content }) => (
-                    <div key={ id } className={ styles.legales__wrapper }>
-                        <div class={ styles.legales__title }>
+                    <div 
+                        key={ id } 
+                        className={ `${styles.legales__wrapper} ${id === 0 ? "pt--xLarge" : ""}` }
+                    >
+                        <div className={ styles.legales__title }>
                             <TextInfo body={ title } />
                         </div>
                         
                         <LegalesContent body={ content } />
                     </div>
                 )) }
+
             </div>
         </section>
     )
--- components/Blocks/Legales/LegalesContent.jsx
+++ components/Blocks/Legales/LegalesContent.jsx
@@ -1,3 +1,4 @@
+import TextReveal from "@/components/UI/Elements/Text/TextReveal"
 import styles from "./LegalesContent.module.scss"
 
 
@@ -8,35 +9,28 @@ const LegalesContent = ({ body }) => {
             return (
                 <div className={ styles.content }>
                     <p className={ `${styles.content__text} text--large` }>
-                        En vertu de l’Article 6 de la Loi n° 2004-575 du 21 juin 2004 pour la confiance dans l’économie numérique, il est précisé dans cet article l’identité des différents intervenants dans le cadre de sa réalisation et de son suivi.
+                        <TextReveal text="En vertu de l’Article 6 de la Loi n° 2004-575 du 21 juin 2004 pour la confiance dans l’économie numérique, il est précisé dans cet article l’identité des différents intervenants dans le cadre de sa réalisation et de son suivi." />
+                    </p>
+                    <p className={ `${styles.content__text} ${styles.content__title} text--large text--opacity` }> 
+                        <TextReveal text="Éditeur du site" />
                     </p>
                     <p className={ `${styles.content__text} text--large` }>
-                        <span className="text--opacity">
-                            Éditeur du site
-                        </span>
+                        <TextReveal text="Le site cyrilgouv.com est édité par : Cyril Gouverneur ayant son siège social à l’adresse suivante : 62 rue diamant, 40600 Biscarrosse." />
                     </p>
                     <p className={ `${styles.content__text} text--large` }>
-                        Le site cyrilgouv.com est édité par : Cyril Gouverneur ayant son siège social à l’adresse suivante : 62 rue diamant, 40600 Biscarrosse.
+                        <TextReveal text="Adresse e-mail de contact : hello@cyrilgouv.com" />
+                    </p>
+                    <p className={ `${styles.content__text} ${styles.content__title} text--large text--opacity` }>
+                        <TextReveal text="Création et réalisation" />
                     </p>
                     <p className={ `${styles.content__text} text--large` }>
-                        Adresse e-mail de contact : hello@cyrilgouv.com
+                        <TextReveal text="Le site cyrilgouv.com a été désigné et développé par Cyril Gouverneur." />
+                    </p>
+                    <p className={ `${styles.content__text} ${styles.content__title} text--large text--opacity` }>
+                        <TextReveal text="Hébergeur du site" />
                     </p>
                     <p className={ `${styles.content__text} text--large` }>
-                        <span className="text--opacity">
-                            Création et réalisation
-                        </span>
-                    </p>
-                    <p className={ `${styles.content__text} text--large` }>
-                        Le site cyrilgouv.com a été désigné et développé par Cyril Gouverneur.
-                    </p>
-                    <p className={ `${styles.content__text} text--large` }>
-                        <span className="text--opacity">
-                            Hébergeur du site
-                        </span>
-                    </p>
-                    <p className={ `${styles.content__text} text--large` }>
-                        Le site cyrilgouv.com est hébergé par : Netlify dont le siège est situé à l’adresse suivante :
-                        44 Montgomery St STE 300, San Francisco, CA 94104 United States
+                        <TextReveal text="Le site cyrilgouv.com est hébergé par : Netlify dont le siège est situé à l’adresse suivante : 44 Montgomery St STE 300, San Francisco, CA 94104 United States" />
                     </p>
                 </div>
             ) 
@@ -44,23 +38,23 @@ const LegalesContent = ({ body }) => {
         case "confidentialite":
             return (
                 <div className={ styles.content }>
-                    <p class={ `${styles.content__text} text--large` }>
-                        Conformément aux dispositions de l’article 5 du Règlement européen 2016/679, la collecte et le traitement des données des utilisateurs du site respectent les principes suivants
+                    <p className={ `${styles.content__text} text--large` }>
+                        <TextReveal text="Conformément aux dispositions de l’article 5 du Règlement européen 2016/679, la collecte et le traitement des données des utilisateurs du site respectent les principes suivants" />
                     </p>
-                    <p class={ `${styles.content__text} text--large` }>
-                        Licéité, loyauté et transparence : les données ne peuvent être collectées et traitées qu’avec le consentement de l’utilisateur propriétaire des données. A chaque fois que des données à caractère personnel seront collectées, il sera indiqué à l’utilisateur que ses données sont collectées, et pour quelles raisons ses données sont collectées.
+                    <p className={ `${styles.content__text} text--large` }>
+                        <TextReveal text="Licéité, loyauté et transparence : les données ne peuvent être collectées et traitées qu’avec le consentement de l’utilisateur propriétaire des données. A chaque fois que des données à caractère personnel seront collectées, il sera indiqué à l’utilisateur que ses données sont collectées, et pour quelles raisons ses données sont collectées." />
                     </p>
-                    <p class={ `${styles.content__text} text--large` }>
-                        Finalités limitées : la collecte et le traitement des données sont exécutés pour répondre à un ou plusieurs objectifs déterminés dans les présentes mentions légales.
+                    <p className={ `${styles.content__text} text--large` }>
+                        <TextReveal text="Finalités limitées : la collecte et le traitement des données sont exécutés pour répondre à un ou plusieurs objectifs déterminés dans les présentes mentions légales." />
                     </p>
-                    <p class={ `${styles.content__text} text--large` }>
-                        Minimisation de la collecte et du traitement des données : seules les données nécessaires à la bonne exécution des objectifs poursuivis par le site sont collectées.
+                    <p className={ `${styles.content__text} text--large` }>
+                        <TextReveal text="Minimisation de la collecte et du traitement des données : seules les données nécessaires à la bonne exécution des objectifs poursuivis par le site sont collectées." />
                     </p>
-                    <p class={ `${styles.content__text} text--large` }>
-                        Conservation des données réduites dans le temps : les données sont conservées pour une durée limitée, dont l’utilisateur est informé. Si la durée de conservation ne peut être communiquée à l’utilisateur.
+                    <p className={ `${styles.content__text} text--large` }>
+                        <TextReveal text="Conservation des données réduites dans le temps : les données sont conservées pour une durée limitée, dont l’utilisateur est informé. Si la durée de conservation ne peut être communiquée à l’utilisateur." />
                     </p>
-                    <p class={ `${styles.content__text} text--large` }>
-                        Intégrité et confidentialité des données collectées et traitées : le responsable du traitement des données s’engage à garantir l’intégrité et la confidentialité des données collectées.
+                    <p className={ `${styles.content__text} text--large` }>
+                        <TextReveal text="Intégrité et confidentialité des données collectées et traitées : le responsable du traitement des données s’engage à garantir l’intégrité et la confidentialité des données collectées." />
                     </p>
                 </div>
             )
