cb89d700ec7e6f4826214f2a2d344e7a1b868917
Cyril Gouv <hello@cyrilgouv.com>
2024-05-11 09:25:42 +0200

    added title more

--- components/Blocks/Testimonials/FeaturedTestimonials.jsx
+++ components/Blocks/Testimonials/FeaturedTestimonials.jsx
@@ -1,8 +1,10 @@
 import TitleSection from "@/components/UI/Elements/Title/TitleSection"
 import SliderTestimonials from "@/components/UI/Sliders/SliderTestimonials"
+import Cta from "@/components/UI/Cta/Cta"
+import TextInfo from "@/components/UI/Elements/Text/TextInfo"
+import Tags from "@/components/UI/Elements/Tags/Tags"
 
 import styles from "./FeaturedTestimonials.module.scss"
-import Cta from "@/components/UI/Cta/Cta"
 
 
 const FeaturedTestimonials = () => {
@@ -11,6 +13,21 @@ const FeaturedTestimonials = () => {
             <div className="container">
                 <div className={ styles.testimonials__title }>
                     <TitleSection text="Clients" />
+
+                    <div className={ styles.testimonials__title__more }>
+                        <div className={ styles.testimonials__title__more__wrapper }>
+                            <Tags 
+                                text="+23 clients" 
+                                position="relative" 
+                                animations={{ delay: .25 }} 
+                            />
+                            <TextInfo 
+                                body="Ils sont entrepreneurs, indépendants, e-commerçants..." 
+                                full={ true } 
+                                className="pt--xSmall"
+                            />
+                        </div>
+                    </div>
                 </div>
 
                 <div className={ styles.testimonials__wrapper }>
