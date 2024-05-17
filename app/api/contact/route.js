858e5a7ba744cd3d9abc3ffc1ec3ae7f261fffd2
Cyril Gouv <hello@cyrilgouv.com>
2024-05-17 11:43:25 +0200

    added nodemailer

--- app/api/contact/route.js
+++ app/api/contact/route.js
@@ -1,8 +1,56 @@
+const nodemailer = require("nodemailer")
+
+
 export async function POST(request) {
     const { fullname, email, subject, budget, brief } = await request.json()
     
     if (fullname && email && subject && budget && brief) {
-        return Response.json({ "message": "sent" })
+
+        const transporter = nodemailer.createTransport({
+            port: process.env.EMAIL_PORT,
+            host: process.env.EMAIL_HOST,
+            auth: {
+              user : process.env.EMAIL_USERNAME,
+              pass: process.env.EMAIL_PASSWORD,
+            },
+            secureConnection: false,
+            tls: { ciphers: "SSLv3" },
+        })
+        
+        const output = `
+            <div>
+              <h2>Informations de contact</h2>
+              <div>Nom complet: ${fullname}</div>
+              <div>Email: ${email}</div>
+              <br />
+              <br />
+              <h2>Sujet du contact</h2>
+              <div>${subject}</div>
+              <div>Budget: ${budget}</div>
+              <br />
+              <br />
+              <h2>Brief</h2>
+              <div>${brief}</div>
+            </div>
+        `
+
+        const mailData = {
+            from: process.env.EMAIL_USERNAME,
+            to: process.env.EMAIL_USERNAME,
+            subject: "Prise de contact 🎉🎊🥳",
+            text: brief,
+            html: output
+        }
+
+        try {
+            await transporter.sendMail(mailData)
+            return Response.json({ "message": "sent" })
+
+        } catch (error) {
+            console.log(error)
+            return Response.json({ "message": "error" })
+        }
+
     } else {
         return Response.json({ "message": "error" })
     }
